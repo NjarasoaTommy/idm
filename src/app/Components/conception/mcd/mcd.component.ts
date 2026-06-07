e1964cb7731a935cc8779b9dd5ea4b1c4309ea00
Njarasoa Tommy <njarasoatommy@gmail.com>
2026-06-07 22:08:31 +0300

    Also take care about relation which has  both input and output connexion.

--- src/app/Components/conception/mcd/mcd.component.ts
+++ src/app/Components/conception/mcd/mcd.component.ts
@@ -32,7 +32,7 @@ export class McdComponent {
         }
       ],
       node_position: { x: 32, y: 32 },
-      node_connection_side: "right",
+      node_output_connection_side: "right",
       node_relation_type: "output",
       node_output_id: "output1"
     },
@@ -55,9 +55,34 @@ export class McdComponent {
         }
       ],
       node_position: { x: 240, y: 32 },
-      node_connection_side: "left",
+      node_input_connection_side: "left",
+      node_output_connection_side: "right",
+      node_relation_type: "both",
+      node_input_id: "input1",
+      node_output_id: "output2"
+    },
+    {
+      node_id: 3,
+      node_title: "Entité 2",
+      node_type: "entity",
+      node_attributes: [
+        {
+          label : "id2",
+          type : 'number'
+        },
+        {
+          label : "attr21",
+          type : 'text'
+        },
+        {
+          label : "attr22",
+          type : 'boolean'
+        }
+      ],
+      node_position: { x: 570, y: 32 },
+      node_input_connection_side: "left",
       node_relation_type: "input",
-      node_input_id: "input1"
+      node_input_id: "input2"
     }
   ]
 
@@ -76,5 +101,9 @@ export class McdComponent {
 
   get input_nodes() {
     return this.node_list.filter(n => n.node_relation_type === 'input');
+  }
+
+  get both_nodes() {
+    return this.node_list.filter(n => n.node_relation_type === 'both');
   }
 }
