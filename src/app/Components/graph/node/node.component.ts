e9e1b8c7dd48629405942b19f1aae7f2463ffe3b
Njarasoa Tommy <njarasoatommy@gmail.com>
2026-06-08 10:43:23 +0300

    Add documentation for the node graph component.

--- src/app/Components/graph/node/node.component.ts
+++ src/app/Components/graph/node/node.component.ts
@@ -8,21 +8,21 @@ import { Component, Input, HostBinding } from '@angular/core';
   styleUrl: './node.component.scss'
 })
 export class NodeComponent {
-  @Input() showNodeForm!: (
+  @Input() showNodeForm!: ( // Used to send the informations about the current node when the node should be updated.
     node_id: number,
     node_type: string,
     node_title: string,
     node_attributes: any,
   ) => void;
   @Input() node_type!: string;
-  @HostBinding('class') get hostClass() { // Class added to app-node directly.
+  @HostBinding('class') get hostClass() { // Class added to app-node(host) directly.
     return `node ${this.node_type}`;
   }
   @Input() node_id!: number;
   @Input() node_title!: string;
   @Input() node_attributes!: any;
 
-  onSetupNode(event: MouseEvent){
+  onSetupNode(event: MouseEvent){ // Called when the user want to update node.
     event.preventDefault();
     this.showNodeForm(this.node_id, this.node_type,this.node_title, this.node_attributes);
   }
