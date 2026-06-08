c88bff6b55aacce30a82810acb9ecdffb9df080e
Njarasoa Tommy <njarasoatommy@gmail.com>
2026-06-08 10:26:35 +0300

    Document the code for mcd component.

--- src/app/Components/conception/mcd/mcd.component.ts
+++ src/app/Components/conception/mcd/mcd.component.ts
@@ -13,17 +13,20 @@ import { Subscription } from 'rxjs';
   styleUrl: './mcd.component.scss'
 })
 export class McdComponent implements OnDestroy {
-  is_node_form_showed: boolean = false;
-  curent_node_id: number = -1;
+  is_node_form_showed: boolean = false; // Control when to show or to hide the node form.
+  // Used to initialize the node form
+  curent_node_id: number = -1; 
   curent_node_type: string = "";
   curent_node_title: string = "";
   curent_node_attributes: any = [];
+
   node_list: any = [];
   connection_list: any = [];
-  node_list_subscription!: Subscription;
-  connection_list_subscription!: Subscription;
+  node_list_subscription!: Subscription; // Used to be aware of node update
+  connection_list_subscription!: Subscription; // Used to be aware of connection update
 
   constructor(private data_service: DataService){
+    // Subscribe for dynamic data(node and connection)
     this.node_list_subscription =  this.data_service.node_list$.subscribe((all_nodes: any) => {
       this.node_list = all_nodes;
     });
@@ -34,10 +37,13 @@ export class McdComponent implements OnDestroy {
   }
 
   show_node_form = (node_id: number, node_type: string, node_title: string, node_attributes: any) => {
+    // Called when the node form should be opened.
     this.curent_node_id = node_id;
     this.curent_node_type = node_type;
     this.curent_node_title = node_title;
     this.curent_node_attributes = node_attributes;
+    
+    // Update all needed data and show the form.
     this.is_node_form_showed = true;
   }
 
@@ -45,6 +51,7 @@ export class McdComponent implements OnDestroy {
     this.is_node_form_showed = false;
   }
 
+  //Methods used to get nodes depends on their type(output, input, both)
   get output_nodes() {
     return this.node_list.filter((n: any) => n.node_relation_type === 'output');
   }
@@ -58,6 +65,7 @@ export class McdComponent implements OnDestroy {
   }
 
   ngOnDestroy(){
+    // Unsubscribe after destroying component.
     this.node_list_subscription.unsubscribe();
     this.connection_list_subscription.unsubscribe();
   }
